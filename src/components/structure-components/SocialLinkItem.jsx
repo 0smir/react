aa0fceba3f921321bb9ff829590ba6b34e3bc6bd
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-02-10 19:47:41 +0200

    remove usless code

--- src/components/structure-components/SocialLinkItem.jsx
+++ src/components/structure-components/SocialLinkItem.jsx
@@ -2,10 +2,8 @@ import  {NavLink} from 'react-router-dom';
 
 export default function SocialLinkItem (props) {
     const {link, name, img_link} = props.mediaItem;
-    console.log('props', props);
     return(
         <li className="footer_social-item">
-            <img src="/images/favicon.png" alt="TEST" />
             <NavLink  className="footer_social-link footer_social-link--facebook" to={link} aria-label={`${name}: link open in a new tab`}>
                 <img className={`logo social-logo social-logo--${name}`} src={img_link} alt={`${name} logo`} />
             </NavLink>
