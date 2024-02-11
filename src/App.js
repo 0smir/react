abeccc34f132d9fb2367692ac9b619cd2425f427
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-02-11 14:12:28 +0200

    add fav-icon to public images to display site icon on tab, remove not used imports, add individual article route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Layout from './Layout';
 import Home from './components/pages/home/Home';
@@ -7,19 +7,14 @@ import FAQ from './components/pages/faq/FAQ';
 import About from './components/pages/about/About';
 import ContactUs from './components/pages/contact_us/ContactUs';
 import Error from './components/pages/error/Error';
-
+import Article from './components/pages/blog/Article';
 
 //styles imports
 import "./styles/reset.css";
 import "./styles/common_styles.css";
 
-
-
-
 function App() {
  
-
-
   return (
     <div className="App">
     
@@ -30,6 +25,7 @@ function App() {
           <Route path="blog" element={<Blog />} />
           <Route path="faq" element={<FAQ />} />
           <Route path="contact-us" element={<ContactUs />} />
+          <Route path="article/:id" element={<Article />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
