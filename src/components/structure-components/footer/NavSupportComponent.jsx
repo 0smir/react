34cc4e43bfb3bd10e299323bc017b253b27de47a
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-03 14:35:34 +0200

    add css-class for component

--- src/components/structure-components/footer/NavSupportComponent.jsx
+++ src/components/structure-components/footer/NavSupportComponent.jsx
@@ -9,7 +9,7 @@ export default function NavSupportComponent () {
         <div className="footer-nav__links-list support-links__wrapper">
             <h2 className="title footer-nav__title support-links__title">Support</h2>
             <ul className="support-links__list">
-                {NavigationItems.map(navItem => <NavItem key={navItem.id} item={navItem} />)}
+                {NavigationItems.map(navItem => <NavItem key={navItem.id} item={navItem} className="footer__link" />)}
             </ul>
         </div>
     )
