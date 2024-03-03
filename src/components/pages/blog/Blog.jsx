0b1cd34d1006c1e8271e897a0d0b68f5ca8e8599
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-03 14:45:21 +0200

    add corrects to requests, modified structure of component, add load more button and add loading-logic

--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,37 +1,98 @@
 import {useState, useEffect} from 'react';
-import PostItem from '../../structure-components/PostItem';
+import BlogFilterTabs from '../../structure-components/blog/BlogFilterTabsList';
+import PostItem from '../../structure-components/blog/PostItem';
 import '../../../components/pages/blog/blog.css';
+import '../../../styles/blog_filter.css';
 
 function Blog() {
-    const [postsList, setPostsList] = useState({}),
-            limit = 20;
+    const [postsList, setPostsList] = useState([]),
+          [postTypes, setPostType] = useState([]),
+          [isLoaded, setIsLoaded] = useState(false),
+          [step, setStep] = useState({limit: 20, offset: 0}),
+          [totalItemsCount, setTotalItemsCount]= useState(0);
+
+    
+    useEffect(() => {
+        loadData();
+    }, [step]);
 
     useEffect(() => {
-         fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`, { method: "GET"})
+        fetch(`https://pokeapi.co/api/v2/type/`, { method: "GET" })
         .then((response) => response.json())
         .then((data) => {
-             setPostsList(data.results);
+            setPostType(data.results);
         })
         .catch((error) => console.log(error));
     }, []);
-    
+
+    function loadData(){
+        //get start data
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${step.limit}&offset=${step.offset}`, { method: "GET"})
+        .then((response) => response.json())
+        .then((data) => {
+            let result = data.results;
+            let fetchPromises = result.map((item) => {
+                return fetch(item.url, { method: "GET" })
+                .then((resp) => resp.json())
+                .catch((error) => console.log(error));
+            });
+
+            if(!totalItemsCount) {
+                setTotalItemsCount(data.count);
+            }
+            
+            return Promise.all(fetchPromises);
+        })
+        .then((datainfoArray) => {
+            setPostsList((oldList) => {
+                let prevLoadedItems = [...oldList];
+                console.log('!!!!!!!!!!!!!!');
+                return prevLoadedItems.concat(datainfoArray);
+            });
+            setIsLoaded(true);
+        })
+        .catch((error) => console.log(error));
+    }
+   
+    function onFilterSelect(selectedTypeName) {
+        console.log("onFilterSelect", selectedTypeName);
+    }
+
+    function loadMorePosts() {    
+        console.log('loadMorePosts!!!');
+        setStep((oldParams) => {
+            const newParams = {limit: oldParams.limit + 20, offset: oldParams.offset + 20};
+            // console.log('newParams', newParams);
+            return {...newParams};
+        });
+    }
+
+
     
     return(
         <div className="page blog-page">
             <div className="container">
                 <h1 className="title page-title">Blog</h1>
-               <div>
-                { postsList.length ? (
-                    <div className="posts-list">
-                     { postsList.map((item, index) => <PostItem key={index} item={item} number={index + 1}>
-                     <p>Small additional content to test Children-props</p>
-                     </PostItem>) }
-                    </div>
-                    ) : (
-                        <div className="empty-post">  There are no posts yet! Please, use form to add one!</div>
-                    )
+                {isLoaded && postTypes.length > 0 &&
+                    <BlogFilterTabs types={postTypes} onFilterChange={(type) => onFilterSelect(type)}/>
                 }
-               </div>    
+                <div>
+                    { postsList?.length ? (
+                        <div className="posts-list">
+                        { postsList.map((item, index) => <PostItem key={`${item.id}_${index}`} {...item} />)}
+                        </div>
+                        ) : (
+                            <div className="empty-post">  There are no posts yet! Please, use form to add one!</div>
+                        )
+                    }
+                </div> 
+                
+                {step.limit < totalItemsCount && (
+                    <div className="btn-wrapper">
+                        <button className='button' onClick={loadMorePosts}>show more</button>
+                    </div>  
+                ) }
+ 
             </div>
         </div>
     );            
