3d58d17bdfd00820c1400a7ce44a137985c417ba
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-12 23:22:09 +0200

    add corrects to data-constants, complite css variables

--- src/components/const.js
+++ src/components/const.js
@@ -3,13 +3,23 @@ import GithubMediaLogo from '../images/social/github.svg';
 import TwitterMediaLogo from '../images/social/twitter.svg';
 import InstagramMediaLogo from '../images/social/instagram.svg';
 
+//icons
 import vr_glasses from '../images/advantages/icon_vr_glasses.svg';
 import vr_person from '../images/advantages/icon_vr_person.svg';
 import vr_space from '../images/advantages/icon_vr_space.svg';
 
+import vr_comment from '../images/services/icon_comment.svg';
+import vr_event from '../images/services/icon_event.svg';
+import vr_game_pad from '../images/services/icon_game_pad.svg';
+import vr_light from '../images/services/icon_light.svg';
+import vr_mask from '../images/services/icon_mask.svg';
+import vr_tv from '../images/services/icon_vr_tv.svg';
+
 import client_1 from '../images/clients/client_1.png';
 import client_2 from '../images/clients/client_2.png';
 import client_3 from '../images/clients/client_3.png';
+
+import main_banner from '../images/vr_user.png';
 
 
 // constants.js
@@ -132,18 +142,22 @@ export const HOME_MAIN = [
         title: "Immerse Yourself in Virtual Reality",
         description: "Experience Unforgettable Events in VR. Bring your events to life like never before with our VR services.",
         clients: [client_1, client_2, client_3],
+        banner: main_banner,
         advantages: [
             {
+                id: "expertise",
                 title: "Expertise",
                 icon: vr_glasses,
                 description: "Our team consists of experienced VR developers, designers, and technicians who have a passion for VR and a commitment to delivering quality work and give the best service."
             },
             {
+                id: "customization",
                 title: "Customization",
                 icon: vr_person,
                 description: "Every client is unique, and we believe every VR experience should be too. We'll work with you to create a customized solution that meets your specific needs and goals."
             },
             {
+                id: "service",
                 title: "Service",
                 icon: vr_space,
                 description: "We believe in providing exceptional customer service, from initial consultation to final delivery. Our goal is to ensure you're satisfied with every aspect of your VR experience."
@@ -154,7 +168,7 @@ export const HOME_MAIN = [
 
 export const ABOUT = [
     {   
-        section_title: "ABOUT US",
+        section_name: "ABOUT US",
         title: "Bring your events to life like never before with our VR services.",
         description: "VRNas is a leading provider of VR services for education, entertainment, architecture, and events. Our mission is to bring the power of virtual reality to everyone, allowing them to explore new worlds, learn in new ways, and experience events in a whole new light.",
         checkpoints: ["Cutting-Edge Technology", "Versatile Applications", "Affordable and Accessible"]
@@ -163,44 +177,44 @@ export const ABOUT = [
 
 export const SERVICES = [
     {
-        section_title: "OUR SERVICE",
+        section_name: "OUR SERVICE",
         title: "Our Service",
         description: "We use the latest VR hardware and software to create high-quality VR experiences that are accessible and affordable. Our goal is to provide exceptional customer service and support, and our team is always available to answer any questions and address any concerns you may have.",
         services_list: [
             {
                 id:"development",
                 title: "VR Development",
-                icon: vr_space,
+                icon: vr_tv,
                 description: "From concept to creation, our team of VR developers will bring your vision to life."
             },
             {
                 id:"design",
                 title: "VR Design",
-                icon: vr_space,
+                icon: vr_light,
                 description: "Our talented VR designers will create immersive and engaging environments that will captivate your audience."
             },
             {
                 id:"consulting",
                 title: "VR Consulting",
-                icon: vr_space,
+                icon: vr_comment,
                 description: "Our VR consultants will work with you to ensure that your VR experience meets your goals and exceeds your expectations."
             },
             {
                 id:"games",
                 title: "VR Games",
-                icon: vr_space,
+                icon: vr_game_pad,
                 description: "We offer a wide selection of VR games that are suitable for players of all ages and skill levels."
             },
             {
                 id:"events",
                 title: "VR Events",
-                icon: vr_space,
+                icon: vr_event,
                 description: "Make your next event unforgettable with our VR event services."
             },
             {
                 id:"entertainment",
                 title: "VR Entertainment",
-                icon: vr_space,
+                icon: vr_mask,
                 description: "reate a VR escape room, or offer VR experiences at a theme park, we have the expertise and experience to make it happen."
             }
         ]
@@ -209,7 +223,7 @@ export const SERVICES = [
 
 export const ADVANTAGES = [
     {
-        section_title: "WHY CHOOSE US",
+        section_name: "WHY CHOOSE US",
         title: "Why Choose Us for Your VR Needs",
         advantages: [
             {
@@ -230,4 +244,3 @@ export const ADVANTAGES = [
         ]
     }
 ];
-
