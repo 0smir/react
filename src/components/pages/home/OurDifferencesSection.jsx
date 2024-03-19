06e10771ba4d00d320c4c59b16bb2c2288d31a7b
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-19 13:13:52 +0200

    complite structure and styles for our_difference section

--- src/components/pages/home/OurDifferencesSection.jsx
+++ src/components/pages/home/OurDifferencesSection.jsx
@@ -1,26 +1,28 @@
 import {ADVANTAGES} from "../../const";
 import Accordion from "../../structure-components/common/Accordion";
-
-
+import SectionBanner from "../../structure-components/common/SectionBanner";
+import main_banner from '../../../images/vr_backgrounds/vr_danser.png';
+import banner_small from '../../../images/vr_user_small.jpg';
+import "./_our_difference.scss";
 
 function OurDifferencesSection() {
     const {section_name, title, advantages} = ADVANTAGES[0];
     
     return(
-        <div className="section section--advantages">
+        <div className="section section--our-difference our-difference">
             <div className="container">
-                <div className="section__title-wrapper">
-                    <h1 className="section__name">{section_name}</h1>
-                    <h2 className="title section__title">{title}</h2>
+                <div className="our-difference__content-wrapper">
+                    <div className="our-difference__content">
+                        <div className="section__title-wrapper">
+                            <h1 className="section__name">{section_name}</h1>
+                            <h2 className="title section__title">{title}</h2>
+                        </div>
+                        <Accordion className="our-difference" dataInfo={advantages} />
+                    </div>
+                    <SectionBanner main_banner={main_banner} main_banner_alt_text={section_name}  small_banner={banner_small} className="our-difference" />
                 </div>
-                <div className="section__content">
-                    <Accordion className="our-difference" dataInfo={advantages} />
-                </div>
-
             </div>
-
         </div>
-
     );
 }
 
