2fdaf6fb48c58e63325205079981107bba9a6afa
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-21 00:08:18 +0200

    add subscribe form to Error and FAQ pages

--- src/components/pages/faq/FAQ.jsx
+++ src/components/pages/faq/FAQ.jsx
@@ -1,10 +1,14 @@
 import PageTitle from "../../structure-components/common/PageTitle";
+import SubscribeForm from "../../structure-components/common/SubscribeForm";
+
 
 export default function FAQ () {
     return (
         <div className="page faq-page">
             <div className="container faq-container">
                 <PageTitle text={'FAQ'}/>
+
+                <SubscribeForm />
             </div>
         </div>
     )
