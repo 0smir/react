b38ff78f53e94b79813cd126907ef728337ff005
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-21 18:27:37 +0200

    add corrects to error page adaptivity

--- src/components/pages/error/Error.jsx
+++ src/components/pages/error/Error.jsx
@@ -1,28 +1,37 @@
 import  {Link} from 'react-router-dom';
 import SubscribeForm from "../../structure-components/common/SubscribeForm";
 import SectionTitle from "../../structure-components/common/SectionTitle";
+import SectionBanner from "../../structure-components/common/SectionBanner";
+
+import "./error.scss";
+import error_img from "../../../images/error_bg.png";
 
 export default function Error () {
     return (
         <div className="page error-page">
-            <div className="container">
-                <SectionTitle className="error" section_name="404 error" title="Page Not Found" />
-                <div className="page_content">
-                <p>Oops! It looks like the page you were looking for is not here. Here are some possible reasons why:</p>
-                    <ul>
-                        <li>The page may have been moved or deleted.</li>
-                        <li>You may have mistyped the URL.</li>
-                        <li>There may be a temporary problem with our server.</li>
-                    </ul>
-                    <p>You can try the following options to find what you're looking for:</p>
-                    <ul>
-                        <li>Check the URL for typos or errors and try again.</li>
-                        <li>Go back to our homepage and browse from there.</li>
-                    </ul>
+            <div className="container error-page__container">
+                <div className="error-page__content-wrapper">
+                    <div className="error-page__content">
+                        <SectionTitle className="error" section_name="404 error" title="Page Not Found" />
+                        <p className='text'>Oops! It looks like the page you were looking for is not here. Here are some possible reasons why:</p>
+                        <ul className="error__description-list">
+                            <li className="error__description-item">The page may have been moved or deleted.</li>
+                            <li className="error__description-item">You may have mistyped the URL.</li>
+                            <li className="error__description-item">There may be a temporary problem with our server.</li>
+                        </ul>
+                        <p className='text'>You can try the following options to find what you're looking for:</p>
+                        <ul className="error__description-list">
+                            <li className="error__description-item">Check the URL for typos or errors and try again.</li>
+                            <li className="error__description-item">Go back to our homepage and browse from there.</li>
+                        </ul>
 
-                    <p>If you believe there's an issue with our website, please contact us using the information provided on our contact page.</p>
-                    <Link to="/">Back home</Link>
+                        <p className='text'>If you believe there's an issue with our website, please contact us using the information provided on our contact page.</p>
+                        <Link to="/">Back home</Link>
+                    </div>
+                    <SectionBanner main_banner={error_img} main_banner_alt_text="404 error" className="error" light={false} darck_bg={false}/>
                 </div>
+            </div>
+            <div className="container">
                 <SubscribeForm />   
             </div>
         </div>
--- src/components/structure-components/common/SectionBanner.jsx
+++ src/components/structure-components/common/SectionBanner.jsx
@@ -1,10 +1,11 @@
 import "../../../styles/_section_banner.scss";
+
  
-function SectionBanner({main_banner, main_banner_alt_text,  small_banner, className}) {
+function SectionBanner({main_banner, main_banner_alt_text,  small_banner, className, light=true, darck_bg=true}) {
     return(
-        <div className={`section__banner ${className}__banner light-box`}>
+        <div className={`section__banner ${className}__banner ${light ? 'light-box' : ''} ${darck_bg ? 'darck-bg' : ''}`}>
             <img src={main_banner} alt={main_banner_alt_text} className={`image-fluid section__banner-img--main ${className}__banner-img--main`}/>
-            <img src={small_banner} alt="VR" className={`section__banner-img--small ${className}__banner-img--small`}/>
+            {small_banner && (<img src={small_banner} alt="VR" className={`section__banner-img--small ${className}__banner-img--small`}/>)}
         </div>
     )
 }
