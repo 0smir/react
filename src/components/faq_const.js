765665491949c122cc0502cc765b94f5d66715c6
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-23 17:34:40 +0200

    add FAQ page contebt to constants, add structure and some styles for FAQ page

---
+++ src/components/faq_const.js
@@ -0,0 +1,74 @@
+export const API_FAQ = [
+    {
+        id: "1",
+        category: "vr_service",
+        title: "What is virtual reality?",
+        description: "Virtual reality (VR) is a technology that uses headsets or other devices to create a simulated environment. Users can interact with this environment as if it were real, allowing for immersive experiences in gaming, education, training, and more.",
+    },
+    {
+        id: "2",
+        category: "vr_service",
+        title: "What equipment is needed for VR?",
+        description: "Basic VR equipment includes a VR headset (HMD) and often motion controllers. More advanced setups may involve sensors for room-scale tracking and powerful computers.",
+    },
+    {
+        id: "3",
+        category: "vr_service",
+        title: "How is VR used for entertainment?",
+        description: "VR offers immersive gaming experiences, interactive storytelling, virtual tours of places, and immersive videos, providing users with a sense of presence in virtual worlds",
+    },
+    {
+        id: "4",
+        category: "vr_pricing",
+        title: "How much does VR equipment typically cost?",
+        description: "Prices vary depending on the quality and type of equipment. Entry-level VR headsets can start from a couple hundred dollars, while high-end setups with advanced features can cost several thousand dollars.",
+    },
+    {
+        id: "5",
+        category: "vr_service",
+        title: "What are the main types of VR?",
+        description: "There are mainly two types of VR: tethered VR, which requires a computer or game console to power the experience, and standalone VR, which operates independently without external devices.",
+    },
+    {
+        id: "6",
+        category: "vr_service",
+        title: "What are some applications of VR beyond gaming?",
+        description: "VR is used in education for immersive learning experiences, in healthcare for training simulations and therapeutic applications, in architecture and design for virtual prototyping, and in enterprise for virtual meetings and remote collaboration.",
+    },
+    {
+        id: "7",
+        category: "vr_service",
+        title: "How does VR technology simulate realistic environments?",
+        description: "VR technology uses high-resolution displays, motion tracking sensors, and spatial audio to create immersive environments that respond to users' movements, providing a sense of presence and realism.",
+    },
+    {
+        id: "8",
+        category: "vr_service",
+        title: "What are the potential risks or drawbacks of using VR?",
+        description: "Some potential risks include motion sickness, eye strain, and disorientation, especially if the VR experience does not match the user's physical movements accurately.",
+    },
+    {
+        id: "9",
+        category: "vr_social",
+        title: "How is VR shaping the future of industries?",
+        description: "Virtual reality (VR) is profoundly shaping the future of industries across the board by offering innovative solutions that revolutionize various aspects of business operations and consumer experiences. In sectors such as healthcare, VR is enabling advanced medical training through realistic simulations, improving patient care with immersive therapy sessions, and even assisting in surgical procedures through virtual preoperative planning. In education, VR is transforming the learning experience by providing interactive simulations that enhance comprehension and retention of complex subjects, allowing students to explore historical events or scientific concepts in a captivating and memorable way. Moreover, in industries like architecture and design, VR facilitates the creation of virtual prototypes, allowing architects and designers to visualize and refine projects before construction begins, leading to more efficient and cost-effective designs. Additionally, in the field of entertainment and tourism, VR offers unparalleled immersive experiences, allowing users to visit exotic locations, attend live events, or participate in thrilling adventures without leaving their homes. Furthermore, in manufacturing and engineering, VR is streamlining product development processes by enabling virtual prototyping, collaborative design reviews, and assembly simulations, leading to faster time-to-market and reduced costs. Overall, the widespread adoption of VR is poised to revolutionize industries by unlocking new opportunities for innovation, enhancing productivity, and reshaping the way businesses engage with customers and stakeholders.",
+    },
+    {
+        id: "10",
+        category: "vr_social",
+        title: "What are the social implications of widespread VR adoption?",
+        description: "The widespread adoption of virtual reality (VR) could lead to profound social implications, blurring the lines between physical and virtual realities. It may alter how people interact, communicate, and form relationships, potentially impacting social norms and behaviors. Additionally, VR adoption raises concerns about privacy, identity, and the ethical use of immersive technologies in society.",
+    },
+    {
+        id: "11",
+        category: "vr_service",
+        title: "Can I try out your VR services before I commit?",
+        description: "Certainly! Before committing, we offer demo sessions where you can experience our VR services firsthand. These sessions allow you to explore various virtual environments, interact with different VR applications, and understand the potential benefits for your specific needs.",
+    },
+    {
+        id: "12",
+        category: "vr_service",
+        title: "What types of VR services do you offer?",
+        description: "As for types of VR services, they range from entertainment and gaming experiences to educational simulations, virtual tours, training modules, and even therapeutic applications. Each type offers unique immersive experiences tailored to different industries and user preferences, showcasing the versatility and potential of virtual reality technology.",
+    }
+];
--- src/components/pages/faq/FAQ.jsx
+++ src/components/pages/faq/FAQ.jsx
@@ -1,15 +1,33 @@
+import {API_FAQ} from "../../faq_const";
+
 import PageTitle from "../../structure-components/common/PageTitle";
+import SectionTitle from "../../structure-components/common/SectionTitle";
 import SubscribeForm from "../../structure-components/common/SubscribeForm";
+import Accordion from "../../structure-components/common/Accordion";
 
 
-export default function FAQ () {
+// import "./faq.scss";
+
+function FAQ() {
+    
+    const FAQ_LIST = API_FAQ;
+    const categories = ['all', ...new Set(FAQ_LIST.map((item) => item.category))];
+    console.log(categories);
     return (
         <div className="page faq-page">
             <div className="container faq-container">
                 <PageTitle text={'FAQ'}/>
-
+                <div className="page__content-wrapper">
+                    <SectionTitle className="faq" section_name="FAQ" title="Frequently Asked Questions"/>
+                    <p className="description">
+                        At VRNas, we want to make sure that you have all the information you need to make informed decisions about our VR services. Here are some of the most common questions we receive:
+                    </p>
+                    <Accordion className="faq" dataInfo={FAQ_LIST}/>
+                </div>
                 <SubscribeForm />
             </div>
         </div>
     )
 }
+
+export default FAQ;
