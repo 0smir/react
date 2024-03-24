91d2f6373f2e693fe54238107e5b4755646c9743
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-24 22:45:00 +0200

    complite variables, add corrects to accordion styles, change structure of accordion items

--- src/components/structure-components/common/AccordionItem.jsx
+++ src/components/structure-components/common/AccordionItem.jsx
@@ -9,19 +9,20 @@ function AccordionItem({className, activeID, item, toggleContent}) {
     console.log(activeID);
     return(
         <>
-            <div className={`accordion-item ${className}__accordion-item`}>
+            <div className={`accordion-item ${className}__accordion-item ${(isExpanded) ? 'active' : ''}`}>
                 <header className={`accordion-header ${className}__accordion-header`} id={`heading-${id}`}>
                     <Button hendleBtnClick={() => toggleContent(id)} className={`accordion-button ${className}__accordion-button`} aria-expanded={isExpanded} aria-controls={`collapse-${id}`}>
                         <span>{title}</span> 
                         <span className={`accordion-arrow ${(!isExpanded) ? '' : 'expand_less'}`}>&rsaquo;</span>
                     </Button>
                 </header>
-            </div>
-            <div id={`collapse-${id}`} className={`accordion-collapse ${className}__accordion-collapse collapse ${(isExpanded) ? 'show' : ''}`} aria-labelledby={`heading-${id}`}>
-                <div className={`accordion-body ${className}__accordion-body`}>
-                    <p>{description}</p>
+                <div id={`collapse-${id}`} className={`accordion-collapse ${className}__accordion-collapse collapse ${(isExpanded) ? 'show' : ''}`} aria-labelledby={`heading-${id}`}>
+                    <div className={`accordion-body ${className}__accordion-body`}>
+                        <p>{description}</p>
+                    </div>
                 </div>
             </div>
+
         </>
     )
 }
