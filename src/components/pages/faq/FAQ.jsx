eafa7a6ca5d067caba3252fb33454187775c0e93
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-24 22:43:08 +0200

    add corrects to structure of FAQ page, complite styles

--- src/components/pages/faq/FAQ.jsx
+++ src/components/pages/faq/FAQ.jsx
@@ -4,25 +4,52 @@ import PageTitle from "../../structure-components/common/PageTitle";
 import SectionTitle from "../../structure-components/common/SectionTitle";
 import SubscribeForm from "../../structure-components/common/SubscribeForm";
 import Accordion from "../../structure-components/common/Accordion";
+import Button from "../../structure-components/common/Button";
 
-
-// import "./faq.scss";
+import "./faq.scss";
+import { useState } from "react";
 
 function FAQ() {
-    
-    const FAQ_LIST = API_FAQ;
-    const categories = ['all', ...new Set(FAQ_LIST.map((item) => item.category))];
-    console.log(categories);
+    const faqList = API_FAQ;
+    const categories = ["all", ...new Set(faqList.map((item) => item.category))];
+    const [questionsList, setQuestionsList] = useState(faqList);
+    const [activeCategory, setActiveCategory] = useState('all');
+        
+
+    function filterQuestions(category) {
+        setActiveCategory(category);
+        const newQuestionsList = faqList.filter((item) => item.category === category); 
+       
+        if(category === "all"){
+            setQuestionsList(faqList);
+            return;
+        }
+        setQuestionsList(newQuestionsList);
+    }
     return (
         <div className="page faq-page">
             <div className="container faq-container">
                 <PageTitle text={'FAQ'}/>
-                <div className="page__content-wrapper">
+                <div className="page__content-wrapper faq-page__content-wrapper">
                     <SectionTitle className="faq" section_name="FAQ" title="Frequently Asked Questions"/>
-                    <p className="description">
+                    <p className="faq__description">
                         At VRNas, we want to make sure that you have all the information you need to make informed decisions about our VR services. Here are some of the most common questions we receive:
                     </p>
-                    <Accordion className="faq" dataInfo={FAQ_LIST}/>
+                    <div className="faq__filter-wrapper">
+                        {categories.length && (categories.map((item) =>{
+                            return (
+                                    <Button className={`btn btn--outline faq__btn-filter ${activeCategory === item ? 'active' : ''}`} 
+                                            key={item} 
+                                            hendleBtnClick={()=>filterQuestions(item)}
+                                    > 
+                                            {item}
+                                    </Button>
+                                )
+                        }))}
+                    </div>
+                    <div className="faq__accordion-wrapper">
+                        <Accordion className="faq" dataInfo={questionsList} activeItemId="1"/>
+                    </div>
                 </div>
                 <SubscribeForm />
             </div>
