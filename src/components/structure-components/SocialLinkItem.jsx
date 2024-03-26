3bf6be6edd6a8ad972f8313570bb7efe55ac445a
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-03-26 15:49:56 +0200

    add location props  to social-component, add corrects  to class names

--- src/components/structure-components/SocialLinkItem.jsx
+++ src/components/structure-components/SocialLinkItem.jsx
@@ -1,10 +1,11 @@
 import  {NavLink} from 'react-router-dom';
 
-export default function SocialLinkItem (props) {
-    const {link, name, img_link} = props.mediaItem;
+export default function SocialLinkItem ({mediaItem, className}) {
+    const {link, name, img_link} = mediaItem;
+    
     return(
-        <li className="footer-social__item">
-            <NavLink  className="footer-social__link footer-social__link--facebook" to={link} aria-label={`${name}: link open in a new tab`}>
+        <li className={`social__item ${className}-social__item`}>
+            <NavLink  className={`social__link ${className}-social__link ${className}-social__link--${name}`} to={link} aria-label={`${name}: link open in a new tab`}>
                 <img className={`logo social-logo social-logo--${name}`} src={img_link} alt={`${name} logo`} />
             </NavLink>
         </li>
--- src/components/structure-components/SocialLinks.jsx
+++ src/components/structure-components/SocialLinks.jsx
@@ -1,15 +1,15 @@
 import {API_URLs_SOCIAL} from '../const';
 import SocialLinkItem from './SocialLinkItem';
-import './../../styles/_footer_social.scss';
+import './../../styles/_social_media.scss';
 
 
-export default function Socialmedia () {
+export default function Socialmedia ({location}) {
     const socialLinksList = API_URLs_SOCIAL;
     
     return (
-        <ul className="footer-social">
+        <ul className={`social ${location}-social`}>
             {socialLinksList.map((mediaItem, index) => 
-                <SocialLinkItem key={index} mediaItem={mediaItem} />
+                <SocialLinkItem key={index} mediaItem={mediaItem} className={location}/>
             )}
         </ul>
     )
