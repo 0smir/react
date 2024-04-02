b295b82a27c20073de7c6d543fffbb33e6f8140c
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-04-02 23:39:44 +0300

    add corrects for points list - remove useless wrapper of points

--- src/components/structure-components/PointsList.jsx
+++ src/components/structure-components/PointsList.jsx
@@ -1,21 +1,25 @@
+import "../../styles/_advantages_list.scss";
+
 
 function PointsList({className, itemList}) {
    
     return(
-        <ul className={`${className}__list`}>
-           {itemList.map(item => {
-                let {id, icon, title, description} = item;
-                
-                return (
-                    <li className={`${className}__list-item ${className}__item`} key={id}>
-                        <img src={icon} alt={title} className={`${className}__icon`} />
-                        <h3 className={`${className}__title`}>{title}</h3>
-                        <p className={`${className}__description-text`}>{description}</p>
-                    </li>
-                )
-           })}                 
+        <div className={`advantages__wrapper ${className}-advantages__wrapper`}>
+            <ul className={`${className}__list`}>
+                {itemList.map(item => {
+                        let {id, icon, title, description} = item;
+                        
+                        return (
+                            <li className={`${className}__list-item ${className}__item`} key={id}>
+                                <img src={icon} alt={title} className={`${className}__icon`} />
+                                <h3 className={`${className}__title`}>{title}</h3>
+                                <p className={`${className}__description-text`}>{description}</p>
+                            </li>
+                        )
+                })}        
+            </ul>         
+        </div>
 
-        </ul>
     );
 }
 export default PointsList;
