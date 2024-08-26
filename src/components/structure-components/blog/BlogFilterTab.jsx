f239c436a2eb89c82e0b93ff245012af715aa5a4
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-08-26 13:31:05 +0300

    add condition for render of blogFilter tab items, add corrects for styles of labels

--- src/components/structure-components/blog/BlogFilterTab.jsx
+++ src/components/structure-components/blog/BlogFilterTab.jsx
@@ -1,18 +1,20 @@
 import Button from "../common/Button";
 
 function BlogFilterTab({name, url, onTypeSelect}) {
-
+    if(name !== 'unknown'){
     return (
-        <li className="filter__content-item">
-            <Button hendleBtnClick={onTypeSelect} 
-                    className={`filter__btn filter__btn--${name}`}
-                    data-type-name={name}
-                    data-url={url}
-            >
-                {name}
-            </Button>
-        </li>
-    );
+            <li className="filter__content-item">
+                <Button hendleBtnClick={onTypeSelect} 
+                        className={`filter__btn filter__btn--${name}`}
+                        data-type-name={name}
+                        data-url={url}
+                >
+                    {name}
+                </Button>
+            </li>
+        );
+    }
+    
 
 }
 
